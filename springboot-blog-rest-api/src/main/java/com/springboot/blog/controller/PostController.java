4371e225ccf8c67ca8ccec8ec0eb34b80c98d8d4
asing923 <asing923@uwo.ca>
2024-02-11 20:32:58 -0500

    Implemented Spring Security, JWT Authentication and Migrated codebase to updated Spring Boot

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
@@ -6,9 +6,10 @@ import com.springboot.blog.service.PostService;
 import com.springboot.blog.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -22,6 +23,7 @@ public class PostController {
     }
 
     // create blog post rest api
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
@@ -45,6 +47,7 @@ public class PostController {
     }
 
     // update post by id rest api
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id){
 
@@ -54,6 +57,7 @@ public class PostController {
     }
 
     // delete post rest api
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id){
 
